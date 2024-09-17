1160b2e8ad6dc9fb6d7b1a866e2a2b438a564221
abdullah97-pro <abdullahahmadi019@gmail.com>
2024-09-17 16:48:34 +0430

    javascript codes for navbar on mobile base

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-const title = document.getElementById('title');
-title.innerHTML = 'Hello';
+const navbar = document.getElementById('navbar-collapse');
+const btntoggler = document.getElementById('navbar-toggler');
+const discard = document.getElementById('discard');
+
+btntoggler.addEventListener('click', () => {
+  navbar.classList.add('active');
+});
+
+discard.addEventListener('click', () => {
+  navbar.classList.remove('active');
+});
